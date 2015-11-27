6d86797c1e986784201ef1ce54b11093702df1dd
lukasikp <lukasik.paulina@gmail.com>
2015-11-27 13:15:30 +0100

    funkcja zmieniająca widocznosc stron- zdarzenie click

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,41 @@
 $(document).ready(function() {
-	// $("#menu-icon").on('click', function() {
-	// 	$("#show-menu").toggle({left: "0px"});
-	// });
 
 
-$("#menu-icon").on('click', function(e){
-	e.preventDefault();
-    if ( $(this).hasClass("hide") ) {
-		$("#show-menu").stop().animate({left:"-100%"}, 800);	    					
-    } else {
-        $("#show-menu").stop().animate({left:"0px"}, 800);
-    }
-    $(this).toggleClass("hide");
-	return false;
-});
+/*funkcja wysuwajaca menu*/
+
+	$("#menu-icon").on('click', function(e){
+		e.preventDefault();
+	    if ( $(this).hasClass("hide") ) {
+			$("#show-menu").stop().animate({left:"-100%"}, 800);	    					
+	    } else {
+	        $("#show-menu").stop().animate({left:"0px"}, 800);
+	    }
+	    $(this).toggleClass("hide");
+		return false;
+	});
+
+
+/*funkcja zmieniajaca strony*/
+
+	$(".icon_downup .up").on('click', function(){
+		var visibleid = $('.visible').attr('id')
+		if (visibleid=="container1"){
+			$("#container1").removeClass('visible').addClass('disable');
+			$("#container5").removeClass('disable').addClass('visible');
+		}
+		$('.visible').removeClass('visible').addClass('disable').prev().removeClass('disable').addClass('visible');
+		
+	});
+
+	$(".icon_downup .down").on('click', function(){
+		$('.visible').removeClass('visible').addClass('disable').next().removeClass('disable').addClass('visible');
+		var visibleid = $('.visible').attr('id')
+		if (visibleid=="container5"){
+			$("#container5").removeClass('visible').addClass('disable');
+			$("#container1").removeClass('disable').addClass('visible');
+		}
+	});
+
+
 
 });
